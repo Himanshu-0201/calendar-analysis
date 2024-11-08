b3f8c995479d36f8211fee0402eb697336b143ec
HIMANSHU NAGAR <himanshu18nagar@gmail.com>
2024-11-08 17:20:22 +0530

    make profile icon to the first character of user name

--- frontend/src/components/Nav/Avatar/Avatar.js
+++ frontend/src/components/Nav/Avatar/Avatar.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import DropDown from "../DropDown/DropDown";
 import "./Avatar.scss";
+import { useSelector } from "react-redux";
 
 const Avatar = () => {
 
 
     const [isOpen, setIsOpen] = useState(false);
 
-    // console.log(isOpen);
+    const userName = useSelector(state => state.userInfo.name);
+    const formattedFirstChar = userName ? userName.charAt(0).toUpperCase() : '';
+
 
     const toggle = () => {
         setIsOpen(pre => !pre);
@@ -26,7 +29,7 @@ const Avatar = () => {
                 <div className="_avatar relative inline-flex items-center justify-center w-10 h-10 overflow-hidden rounded-full"
                     onClick={toggle}
                 >
-                    <span className="font-medium text-gray-600 dark:text-gray-300">H</span>
+                    <span className="font-medium text-gray-600 dark:text-gray-300">{formattedFirstChar}</span>
                 </div>
 
                 {isOpen && (<DropDown
