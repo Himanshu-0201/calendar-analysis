66d5d7dc03d7f105f6bf2841438bd2a73a6be957
HIMANSHU NAGAR <himanshu18nagar@gmail.com>
2024-12-02 09:48:56 +0530

    removed some commented lines

--- backend/config.js
+++ backend/config.js
@@ -6,7 +6,6 @@ dotenv.config();
 // export const HOST_PORT = 8000;
 export const HOST_PORT = process.env.PORT;
 
-// export const mongoDBUrl = "mongodb+srv://nagarhimanshu1802:P5T4HTsDucDHD3u1@cluster0.hw9ya.mongodb.net/"
 export const mongoDBUrl = process.env.MONGO_DB_URL;
 
 
