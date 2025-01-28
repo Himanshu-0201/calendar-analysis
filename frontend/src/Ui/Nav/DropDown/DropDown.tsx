59f9ce29a401bfb5d8ac140219dd3619ed587c6c
Himanshu Nagar <nagar.himanshu.1802@gmail.com>
2025-01-28 10:31:53 +0530

    bugFix : made text color black and important, for drop down elements.

--- frontend/src/Ui/Nav/DropDown/DropDown.tsx
+++ frontend/src/Ui/Nav/DropDown/DropDown.tsx
@@ -82,7 +82,7 @@ const DropDown = ({ handleCloseDropDown }) => {
                 </li> */}
                 <li>
                     {openSetting && <Settings onCloseSettings={closeSettings} />}
-                    <p onClick={openSettings} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer ">Settings</p>
+                    <p onClick={openSettings} className="_color block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer ">Settings</p>
                 </li>
                 {/* <li>
                     <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
