b23f948859de4616c284a837c5931420d734b136
Himanshu Nagar <nagar.himanshu.1802@gmail.com>
2025-01-30 23:02:32 +0530

    bugFix : handled favicon.png default brower request

--- backend/routes/browser-default-api.js
+++ backend/routes/browser-default-api.js
@@ -1,7 +1,9 @@
 import express from 'express';
 const route = express.Router();
 
-route.get('/favicon.ico', (req, res) => res.status(204).json({ message: "No favicon, error handled by express js" }));
+route.get('/favicon.ico', (req, res) => res.status(204).json({ message: "No favicon.ico, error handled by express js" }));
+route.get('/favicon.png', (req, res) => res.status(204).json({ message: "No favicon.png, error handled by express js" }));
+
 
 route.get('/', (req, res) => res.status(200).send("You made request to default route"));
 
